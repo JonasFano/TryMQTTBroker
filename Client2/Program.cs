965f99eaead7f7fda1d1cb33f673c5e7dbb73c80
Jonas Fano <et20022@lehre.dhbw-stuttgart.de>
2023-07-26 22:08:52 +0200

    Add project files.

---
+++ Client2/Program.cs
@@ -0,0 +1,40 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+var mqttFactory = new MqttFactory();
+
+using (var mqttClient = mqttFactory.CreateMqttClient())
+{
+    var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+
+    // Setup message handling before connecting so that queued messages
+    // are also handled properly. When there is no event handler attached all
+    // received messages get lost.
+    mqttClient.ApplicationMessageReceivedAsync += e =>
+    {
+        Console.WriteLine("Received application message.");
+        //e.DumpToConsole();
+
+        return Task.CompletedTask;
+    };
+
+    await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+    var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+        .WithTopicFilter(
+            f =>
+            {
+                f.WithTopic("mqttnet/samples/topic/2");
+            })
+        .Build();
+
+    await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+    Console.WriteLine("MQTT client subscribed to topic.");
+
+    
+
+    //Console.WriteLine("Press enter to exit.");
+    Console.ReadLine();
+    
+}
---
+++ ConnectToBroker/Program.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+using MQTTnet;
+using MQTTnet.Client;
+
+var mqttFactory = new MqttFactory();
+
+var mqttClient = mqttFactory.CreateMqttClient();
+// Use builder classes where possible in this project.
+var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+
+// This will throw an exception if the server is not available.
+// The result from this message returns additional data which was sent 
+// from the server. Please refer to the MQTT protocol specification for details.
+var response = await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+Console.WriteLine("The MQTT client is connected.");
+Console.WriteLine(response.ResultCode.ToString());
+
+var applicationMessage = new MqttApplicationMessageBuilder()
+            .WithTopic("samples/temperature/living_room")
+            .WithPayload("Ich liebe dich Kai #NoHomo")
+.Build();
+
+await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+
+await mqttClient.DisconnectAsync();
+
+Console.WriteLine("MQTT application message is published.");
+
+
+
+// Send a clean disconnect to the server by calling DisconnectAsync. Without this the TCP connection
+// gets dropped and the server will handle this as a non clean disconnect (see MQTT spec for details).
+var mqttClientDisconnectOptions = mqttFactory.CreateClientDisconnectOptionsBuilder().Build();
+
+await mqttClient.DisconnectAsync(mqttClientDisconnectOptions, CancellationToken.None);
+
+
+
+
